cdefbe5a0da6bcce10d98f203ebcdda3f6c8ad46
JunichiKawahara <kawahara-j@hi-ad.jp>
2019-04-10 20:58:24 +0900

    First Commit

---
+++ test.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+# https://note.nkmk.me/python-pillow-add-margin-expand-canvas/ 
+
+
+def add_margin(pil_img, top, right, bottom, left, color):
+    width, height = pil_img.size
+    new_width = width + right + left
+    new_height = height + top + bottom
+    result = Image.new(pil_img.mode, (new_width, new_height), color)
+    result.paste(pil_img, (left, top))
+    return result
+
+
+def expand2square(pil_img, background_color):
+    width, height = pil_img.size
+    if width == height:
+        return pil_img
+    elif width > height:
+        result = Image.new(pil_img.mode, (width, width), background_color)
+        result.paste(pil_img, (0, (width - height) // 2))
+        return result
+    else:
+        result = Image.new(pil_img.mode, (height, height), background_color)
+        result.paste(pil_img, ((height - width) // 2, 0))
+        return result
+
+
+im = Image.open('./lena.jpg')
+im_new = expand2square(im, (0, 0, 0)).resize((400, 400))
+im_new.save('./lena_new.jpg', quality=95)
